25835f9c11130f032355c817ab7dab0ee5de222f
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-20 16:24:36 -0400

    update routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420194102) do
+ActiveRecord::Schema.define(version: 20170420192329) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20170420194102) do
     t.integer  "capacity"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
