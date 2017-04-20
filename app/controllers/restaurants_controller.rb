80ae87675d607a5f920d420f3a8497bf4025b922
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-20 17:19:00 -0400

    update restaurant view and routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,14 +3,42 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
-  def show
-
-  end
-
   def new
-
+    @restaurants = Restaurant.new
   end
 
   def edit
+    @restaurants = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurants = Restaurant.new(Restaurant_params)
+
+    if @restaurants.save
+      redirect_to Restaurants_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    @restaurants = Restaurant.find(params[:id])
+
+    if @restaurants.update_attributes(Restaurant_params)
+      redirect_to Restaurant_url(@restaurants)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurants = Restaurant.find(params[:id])
+    @restaurants.destroy
+    redirect_to Restaurants_url
+  end
+
+  private
+  def Restaurant_params
+    params.require(:Restaurant).permit(:name, :address, :phone_number, :description, :capacity)
   end
 end
