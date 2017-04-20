33b18c6b2e1f4f65d4917dd695f1d57504ebceb7
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-20 15:23:46 -0400

    update CategoriesRestaurant Table

--- db/migrate/20170420190427_create_restaurants_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateRestaurantsCategories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :restaurants_categories do |t|
-      t.integer :category_id
-      t.integer :restaurant_id
-    end
-  end
-end
