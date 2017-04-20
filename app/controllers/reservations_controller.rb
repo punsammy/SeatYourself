40f8e4974938e47480f5ff837781cadca81bfdc4
quantumduck <quantum.duck@hotmail.com>
2017-04-20 17:56:38 -0400

    first iteration of form

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,30 @@
 class ReservationsController < ApplicationController
-  def index
-  end
 
   def show
+    @reservation = Reservation.find(params[:id])
+    # if @reservation.user == current_user
+    #
+    # else
+    #
+    # end
   end
 
   def new
+    @reservation = Reservation.new
+    @restaurant_list = Restaurant.all.map do |restaurant|
+      [restaurant.name, restaurant.id]
+    end
+    @today = Time.now
   end
 
-  def edit
+  def create
+    @reservation = Reservation.new(reservation_params)
+    # @reservation.user = current_user
+  end
+
+private
+
+  def reservation_params
+    params.require(:reservation).permit(:restaurant_id, :time, :party_size)
   end
 end
