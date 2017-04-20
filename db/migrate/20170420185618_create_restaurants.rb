dcfb933b539cf5e8fe60a9f2ac5eab9e2fc1931a
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-20 15:19:22 -0400

    add columns for restaurant table

--- db/migrate/20170420185618_create_restaurants.rb
+++ db/migrate/20170420185618_create_restaurants.rb
@@ -1,7 +1,12 @@
 class CreateRestaurants < ActiveRecord::Migration[5.0]
   def change
     create_table :restaurants do |t|
-
+      t.string :name
+      t.string :address
+      t.integer :phone_number
+      t.text :description
+      t.integer :capacity
+      
       t.timestamps
     end
   end
