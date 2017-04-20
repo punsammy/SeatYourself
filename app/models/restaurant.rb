7b23c33dbf597cce34020a6cd9e2b736a3fa4826
quantumduck <quantum.duck@hotmail.com>
2017-04-20 17:27:56 -0400

    fake restaurant maker

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,13 @@ class Restaurant < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :users, through: :reservations
   belongs_to :owner, class_name: "User"
+
+  @@counter = 1
+
+  def self.auto_generate
+    r = create(name: "Fake-#{@@counter}", address: "#{rand(1000)} Fake St.",
+               description: "This is a fake restaurant!", capacity: 100, user_id: 0)
+    @@counter += 1
+  end
+
 end
