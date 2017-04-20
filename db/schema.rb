298e99c60fbbddb42c23d4065eff6cd5bbe2cc79
quantumduck <quantum.duck@hotmail.com>
2017-04-20 15:44:22 -0400

    changed owner_id to user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420193221) do
+ActiveRecord::Schema.define(version: 20170420194102) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20170420193221) do
     t.integer  "capacity"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "owner_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
