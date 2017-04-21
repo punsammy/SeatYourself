c745c9a7470391fbf40243737613b97575f5bb4d
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-21 15:16:18 -0400

    revision of create user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_url, notice: "Signed Up!"
+      redirect_to reservations_url, notice: "Signed Up!"
     else
       render "new"
     end
