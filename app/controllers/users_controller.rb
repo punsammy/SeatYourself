55ba993bb305f7ab976469573c7617870ae0340f
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-21 14:27:40 -0400

    revisions to user forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find (params[:id])
-  end
-
   def new
     @user = User.new
   end
@@ -14,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to products_url, notice: "Signed Up!"
+      redirect_to users_url, notice: "Signed Up!"
     else
       render "new"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,8 @@ Rails.application.routes.draw do
 
   get '/' => 'home#index'
 
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/edit'
-
+  resources :users
   resources :reservations
-
   resources :restaurants
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
