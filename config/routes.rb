f04fb40502c6666edd07cb4afe081fa625e588f0
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-21 16:55:13 -0400

    new user and log in feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get '/' => 'home#index'
 
   resources :users
   resources :reservations
   resources :restaurants
+  resources :sessions, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
