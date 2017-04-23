917022c7098d900137a2de89ab8aaac4eda1d5c5
quantumduck <quantum.duck@hotmail.com>
2017-04-23 19:01:45 -0400

    changed owner association due to the fact foreign key is called user_id, not owner_id

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :users, through: :reservations
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :reservations
 
   @@counter = 1
