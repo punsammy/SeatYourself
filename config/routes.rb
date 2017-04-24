312b6cda821d8deccff5fa25b9dbeedecd6230a7
quantumduck <quantum.duck@hotmail.com>
2017-04-24 17:19:08 -0400

    updated routes to add all reservations resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  get "/restaurants/:id/reservations", to: "reservations#restaurant_index", as: :restaurant_reservations
+  # get "/restaurants/:id/reservations", to: "reservations#restaurant_index", as: :restaurant_reservations
 
   resources :users
-  resources :reservations, only: %i(new show create destroy index)
+  resources :reservations #only: %i(new show create destroy index)
   resources :restaurants
   resources :sessions, only: [:new, :create, :destroy]
 
