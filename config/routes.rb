ed8f97f59d05d467d509b576eae557ccc93dbe89
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-24 15:02:51 -0400

    update seeds

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,10 @@ Rails.application.routes.draw do
 
   get "/restaurants/:id/reservations", to: "reservations#index", as: :restaurant_reservations
 
-
-  
-
   resources :users
   resources :reservations, only: %i(new show create destroy)
   resources :restaurants
   resources :sessions, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@
 # Examples:
 #
 
-restaurant1 = Restaurant.new(name: "Quelque Chose", address: "58 Bon St.", phone_number: 416-555-1234, description: "Fine French Dining", capacity: 40, special: "La Salade Gourmet")
+restaurant1 = Restaurant.new(name: "Quelque Chose", address: "58 Bon St.", phone_number: 416-555-1234, description: "Fine French Dining", capacity: 40, special: "La Salade Gourmet", opening_time: , closing_time: )
 
-restaurant2 = Restaurant.new(name: "Jolly Jiggs Pizzeria", "10 Paisano St.", phone_number: 416-555-9671, description: "Supreme pies!", capacity: 10, special: "Jalapeno Kaboom")
+restaurant2 = Restaurant.new(name: "Jolly Jiggs Pizzeria", "10 Paisano St.", phone_number: 416-555-9671, description: "Supreme pies!", capacity: 10, special: "Jalapeno Kaboom", opening_time: , closing_time: )
