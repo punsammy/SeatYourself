22b9cb6b3996a3a32a20097e1bfa680f3730702e
quantumduck <quantum.duck@hotmail.com>
2017-04-24 15:56:15 -0400

    deleted trailing end

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,3 @@
 restaurant1 = Restaurant.new(name: "Quelque Chose", address: "58 Bon St.", phone_number: "416-555-1234", description: "Fine French Dining", capacity: 40, special: "La Salade Gourmet", opening_time: Restaurant.default_opening_time, closing_time: Restaurant.default_closing_time)
 
 restaurant2 = Restaurant.new(name: "Jolly Jiggs Pizzeria", address: "10 Paisano St.", phone_number: "416-555-9671", description: "Supreme pies!", capacity: 10, special: "Jalapeno Kaboom", opening_time: Restaurant.default_opening_time, closing_time: Restaurant.default_closing_time)
-
-end)
